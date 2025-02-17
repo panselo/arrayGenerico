a777d94e8d2504c11eef8aa862cef834da2f91a3
PANSERI.21255 <panseri.tommaso.studente@itispaleocapa.it>
2025-02-17 13:16:45 +0100

    commit

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,13 @@ public class Main {
         ArrayGenerico<Mobile> vetMob = new ArrayGenerico<>();
         vetMob.add(new Mobile(20, 235.0));
         vetMob.add(new Mobile(10, 46.5));
+        vetMob.toString();
+
+
+        ArrayGenerico<Infisso> vetInf = new ArrayGenerico<>();
+        vetInf.add(new Infisso("legno", 20, 30));
+        vetInf.add(new Infisso("alluminio", 40, 10));
+        vetInf.toString();
 
     }
 }
